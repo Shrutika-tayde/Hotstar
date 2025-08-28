504be5cd49af7cb79c621a7f71ad948fb08bdb40
Shrutika Tayde <shrutikatayade85@gmail.com>
2025-08-28 20:34:23 +0530

    Create main.tf

---
+++ Terraform/main.tf
@@ -0,0 +1,50 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+
+}
+
+# create security group for the ec2 instance
+resource "aws_security_group" "ec2_security_group" {
+  name        = "ec2 security group"
+  description = "allow access on ports 22"
+
+  # allow access on port 22
+  ingress {
+    description = "ssh access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Monitoring server security group"
+  }
+}
+
+resource "aws_instance" "Monitoring_server" {
+  ami             = "ami-00ca32bbc84273381"
+  instance_type   = "t2.medium"
+  security_groups = [aws_security_group.ec2_security_group.name]
+  key_name        = var.key_name
+  tags = {
+    Name : var.instance_name
+  }
+}
